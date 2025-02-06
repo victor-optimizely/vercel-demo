5b7c575f828e06614cc0716cbb2aa76dfe875420
Victor <victor.hernandez@optimizely.com>
2025-02-05 19:23:03 -0500

    initial button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,8 @@ export default function Home() {
           <li>Save and see your changes instantly.</li>
         </ol>
 
+        <button>Click me</button>
+
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
